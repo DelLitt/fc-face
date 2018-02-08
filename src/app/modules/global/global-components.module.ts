d80cfe1bf844aff5dd329f0f8a053716708dfca2
Eduard Karulis <s-ekarulis@aras.com>
2018-02-08 17:34:41 +0300

    HEADER. Global header added

--- src/app/modules/global/global-components.module.ts
+++ src/app/modules/global/global-components.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { GlobalNavigationComponent } from '../../components/global/global-navigation/global-navigation.component';
+import { GlobalHeaderComponent } from '../../components/global/global-header/global-header.component';
 
 @NgModule({
   imports: [
@@ -9,10 +10,12 @@ import { GlobalNavigationComponent } from '../../components/global/global-naviga
     TranslateModule
   ],
   declarations: [
-    GlobalNavigationComponent
+    GlobalNavigationComponent,
+    GlobalHeaderComponent
   ],
   exports: [
-    GlobalNavigationComponent
+    GlobalNavigationComponent,
+    GlobalHeaderComponent
   ]
 })
 export class GlobalComponentsModule { }
