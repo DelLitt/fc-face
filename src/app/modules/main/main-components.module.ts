740febfebe8ae93fcd5e7a174c20621397782d7f
Eduard Karulis <karulis@mail.ru>
2018-02-09 14:54:15 +0300

    MAIN. Slider structure shanged

--- src/app/modules/main/main-components.module.ts
+++ src/app/modules/main/main-components.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainBlockFirstComponent } from '../../components/main/main-block-first/main-block-first.component';
 import { MainSliderComponent } from '../../components/main/main-slider/main-slider.component';
+import { SharedComponentsModule } from '../../modules/shared/shared-components.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedComponentsModule
   ],
-  declarations: [MainBlockFirstComponent, MainSliderComponent],
-  exports: [MainBlockFirstComponent, MainSliderComponent]
+  declarations: [
+    MainBlockFirstComponent,
+    MainSliderComponent
+  ],
+  exports: [
+    MainBlockFirstComponent,
+    MainSliderComponent
+  ]
 })
 export class MainComponentsModule { }
