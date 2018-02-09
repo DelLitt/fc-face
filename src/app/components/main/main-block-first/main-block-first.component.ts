a1b4656567398189cb16cafa6a2ee2fcc5a29f50
Eduard Karulis <karulis@mail.ru>
2018-02-09 17:08:40 +0300

    MAIN. Added logging to first main block component

--- src/app/components/main/main-block-first/main-block-first.component.ts
+++ src/app/components/main/main-block-first/main-block-first.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LogService } from '../../../services/log.service';
 
 @Component({
   selector: 'app-main-block-first',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainBlockFirstComponent implements OnInit {
 
-  constructor() { }
+  constructor(private logger: LogService) { }
 
   ngOnInit() {
+    this.logger.logDebug(`'${(<any>this).constructor.name}' component is being initialized.`);
   }
 
 }
