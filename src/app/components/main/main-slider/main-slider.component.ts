1c00f572b3fc205114dbeaaedd278cadeb0df4a0
Eduard Karulis <karulis@mail.ru>
2018-02-09 17:42:22 +0300

    MAIN. Slider. Added dynamic generation of short publications views

--- src/app/components/main/main-slider/main-slider.component.ts
+++ src/app/components/main/main-slider/main-slider.component.ts
@@ -3,13 +3,14 @@ import { PublicationsRepositoryService } from '../../../services/publications-re
 import { Publication } from '../../../model/publication';
 import { LogService } from '../../../services/log.service';
 
+const MaxLeadLength = 150;
+
 @Component({
   selector: 'app-main-slider',
   templateUrl: './main-slider.component.html',
   styleUrls: ['./main-slider.component.scss']
 })
 export class MainSliderComponent implements OnInit {
-
   private _publications: Array<Publication> = null;
 
   constructor(private publicationsRepository: PublicationsRepositoryService, private logger: LogService) { }
@@ -25,5 +26,10 @@ export class MainSliderComponent implements OnInit {
     }
 
     return this._publications;
+  }
+
+  private getLead(publication: Publication) {
+    const lead: string = (publication.lead || '');
+    return lead.length < MaxLeadLength ? lead : lead.substring(0, MaxLeadLength) + '...';
   }
 }
