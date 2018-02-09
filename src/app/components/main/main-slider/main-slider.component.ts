33866610115c6aedf5ad8166e22120d826eba4a4
Eduard Karulis <karulis@mail.ru>
2018-02-09 17:08:10 +0300

    MAIN. Slider. Implemented loading data from the service

--- src/app/components/main/main-slider/main-slider.component.ts
+++ src/app/components/main/main-slider/main-slider.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PublicationsRepositoryService } from '../../../services/publications-repository.service';
+import { Publication } from '../../../model/publication';
+import { LogService } from '../../../services/log.service';
 
 @Component({
   selector: 'app-main-slider',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainSliderComponent implements OnInit {
 
-  constructor() { }
+  private _publications: Array<Publication> = null;
+
+  constructor(private publicationsRepository: PublicationsRepositoryService, private logger: LogService) { }
 
   ngOnInit() {
+    this.logger.logDebug(`'${(<any>this).constructor.name}' component is being initialized.`);
   }
 
+  public get publications(): Array<Publication> {
+    if (this._publications == null) {
+      this._publications = this.publicationsRepository.getHotPublications();
+      this.logger.logInfo(`'${(<any>this).constructor.name}' loaded publications. Count: ${this.publications.length}.`);
+    }
+
+    return this._publications;
+  }
 }
