f996511533750a4f971cb53cb765c1126c806fda
Eduard Karulis <karulis@mail.ru>
2018-02-12 02:00:07 +0300

    SERVICES. Added new method for getting publication

--- src/app/services/publications-repository.service.ts
+++ src/app/services/publications-repository.service.ts
@@ -7,6 +7,26 @@ export class PublicationsRepositoryService {
 
   constructor(private dataSource: DataSourceService) { }
 
+  public getPubliction(id: number) {
+    const element: any = this.dataSource.getPublication(id);
+    if (!element) { return null; }
+
+    const publication = new Publication();
+    publication.id = element.id;
+    publication.header = element.header;
+    publication.img = element.img;
+    publication.lead = element.lead;
+    publication.title = element.title;
+    publication.hasGallery = element.hasGallery;
+    publication.hasVideo = element.hasVideo;
+    publication.content = element.content;
+    publication.author = element.author;
+    publication.displayDate = new Date(element.displayDate);
+    publication.sortDate = new Date(element.sortDate);
+
+    return publication;
+  }
+
   public getHotPublications(): Array<Publication> {
     const data: Array<Publication> = this.dataSource.getLatestPublications(3) || [];
     if (data.length === 0) { return data; }
