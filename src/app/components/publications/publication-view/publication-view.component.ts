6708f0f12956afebda1867aa6f6b266314dc34b0
Eduard Karulis <karulis@mail.ru>
2018-02-12 02:08:35 +0300

    PUBLICATIONS. Added getting 'id' parameter from url

--- src/app/components/publications/publication-view/publication-view.component.ts
+++ src/app/components/publications/publication-view/publication-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-publication-view',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicationViewComponent implements OnInit {
 
-  constructor() { }
+  private publicationId = 0;
+
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.publicationId = +params['id'];
+   });
   }
 
 }
