b230db1e5bd8eebd2ca18176c32b0162fe951319
Eduard Karulis <karulis@mail.ru>
2018-02-15 02:04:13 +0300

    PUBLICATIONS. Added template for publication add-ons

--- src/app/modules/publications/publication-components.module.ts
+++ src/app/modules/publications/publication-components.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatProgressSpinnerModule } from '@angular/material';
+import { MatProgressSpinnerModule, MatExpansionModule } from '@angular/material';
+import { SharedComponentsModule } from '../../modules/shared/shared-components.module';
+
 
 import { PublicationViewComponent } from '../../components/publications/publication-view/publication-view.component';
 import { PublicationDetailsComponent } from '../../components/publications/publication-details/publication-details.component';
@@ -8,7 +10,9 @@ import { PublicationDetailsComponent } from '../../components/publications/publi
 @NgModule({
   imports: [
     CommonModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatExpansionModule,
+    SharedComponentsModule
   ],
   declarations: [
     PublicationViewComponent,
