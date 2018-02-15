30c515e8fa3f36af493f4ba805ed96826953e975
Eduard Karulis <karulis@mail.ru>
2018-02-15 13:56:58 +0300

    PUBLICATIONS. Added gallery support

--- src/app/components/publications/publication-details/publication-details.component.ts
+++ src/app/components/publications/publication-details/publication-details.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { LogService } from '../../../services/log.service';
 import { PublicationsRepositoryService } from '../../../services/publications-repository.service';
 import { Publication } from '../../../model/publication';
+import { GalleriesRepositoryService } from '../../../services/galleries-repository.service';
+import { Gallery } from '../../../model/gallery/gallery';
+import { GalleryItem } from '../../../model/gallery/galleryItem';
 
 @Component({
   selector: 'app-publication-details',
@@ -11,10 +14,14 @@ import { Publication } from '../../../model/publication';
 export class PublicationDetailsComponent implements OnInit {
 
   private publication: Publication = null;
+  private gallery: Gallery = null;
+  private galleryItems: GalleryItem[] = null;
+
   @Input() public id = 0;
 
   constructor(
     private publicationsRepository: PublicationsRepositoryService,
+    private galleriesRepository: GalleriesRepositoryService,
     private logger: LogService) { }
 
   ngOnInit() {
@@ -29,6 +36,16 @@ export class PublicationDetailsComponent implements OnInit {
     .then(result => {
       this.publication = result;
       this.logger.logInfo(`'${(<any>this).constructor.name}' loaded the publication (id:${this.id}) successfully.`);
+
+      this.loadGallery(this.publication.galleryId);
+    });
+  }
+
+  private loadGallery(id: number) {
+    this.galleriesRepository.getGallery(id)
+    .then(result2 => {
+      this.gallery = result2;
+      this.galleryItems = this.gallery.items;
     });
   }
 
