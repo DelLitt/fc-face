e0a13c38ff664e625a13138f3919f78ec6bdd19f
Eduard Karulis <karulis@mail.ru>
2018-02-16 00:13:34 +0300

    MODEL. Class 'Video' added

---
+++ src/app/model/video.ts
@@ -0,0 +1,11 @@
+export class Video {
+    public id: number;
+    public title: string;
+    public header: string;
+    public img: string;
+    public lead: string;
+    public author: string;
+    public displayDate: Date;
+    public sortDate: Date;
+    public htmlCode: string;
+}
