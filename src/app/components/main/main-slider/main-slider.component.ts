7624d4f5ffb46f32410efad0b097b8140b81f926
Eduard Karulis <karulis@mail.ru>
2018-02-16 02:02:46 +0300

    MAIN. Slider. Small code rule fix

--- src/app/components/main/main-slider/main-slider.component.ts
+++ src/app/components/main/main-slider/main-slider.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'; 
+import { Router } from '@angular/router';
 import { PublicationsRepositoryService } from '../../../services/publications-repository.service';
 import { Publication } from '../../../model/publication';
 import { LogService } from '../../../services/log.service';
