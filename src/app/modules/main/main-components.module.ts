29bcdb3ec784812529567ec22c2df3484c97345a
Eduard Karulis <karulis@mail.ru>
2018-02-16 23:41:36 +0300

    MAIN. Removed main first block component

--- src/app/modules/main/main-components.module.ts
+++ src/app/modules/main/main-components.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material';
 import { TranslateModule } from '@ngx-translate/core';
 import { HomeViewComponent } from '../../components/main/home-view/home-view.component';
-import { MainBlockFirstComponent } from '../../components/main/main-block-first/main-block-first.component';
 import { MainSliderComponent } from '../../components/main/main-slider/main-slider.component';
 import { SharedComponentsModule } from '../../modules/shared/shared-components.module';
 import { DataSourceService, FakeDataSourceService } from '../../services/data-source.service';
@@ -25,12 +24,10 @@ import { PublicationsRepositoryService } from '../../services/publications-repos
   ],
   declarations: [
     HomeViewComponent,
-    MainBlockFirstComponent,
     MainSliderComponent
   ],
   exports: [
     HomeViewComponent,
-    MainBlockFirstComponent,
     MainSliderComponent
   ]
 })
