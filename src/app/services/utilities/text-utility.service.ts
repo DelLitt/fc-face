50d1b77d488448f226ef85a28464dff9d1f2c131
Eduard Karulis <karulis@mail.ru>
2018-02-21 01:39:00 +0300

    UTILITY. Added method 'cropWithEllipsis' to text utility

--- src/app/services/utilities/text-utility.service.ts
+++ src/app/services/utilities/text-utility.service.ts
@@ -9,4 +9,9 @@ export class TextUtilityService {
     return text.substr(0, index) + value + text.substr(index);
   }
 
+  public cropWithEllipsis(text: string, maxLenght: number = 64): string {
+    if (!text) { return ''; }
+    return text.length < maxLenght ? text : text.substring(0, maxLenght) + '...';
+  }
+
 }
