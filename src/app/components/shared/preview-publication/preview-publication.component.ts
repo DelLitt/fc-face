94090cd8043fc8f1bc0aa22b5f95dbccd9972d5d
Eduard Karulis <karulis@mail.ru>
2018-02-21 01:39:55 +0300

    PREVIEW PUBLICATION. Layout and style changed

--- src/app/components/shared/preview-publication/preview-publication.component.ts
+++ src/app/components/shared/preview-publication/preview-publication.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { LogService } from '../../../services/log.service';
 import { Entity } from '../../../model/entity';
+import { TextUtilityService } from '../../../services/utilities/text-utility.service';
 
 @Component({
   selector: 'app-preview-publication',
@@ -14,10 +15,23 @@ export class PreviewPublicationComponent implements OnInit {
   @Input() public routePart: string;
 
   constructor(
+    private textUtility: TextUtilityService,
     private logger: LogService
   ) { }
 
   ngOnInit() {
+  }
+
+  private get title(): string {
+    return this.item
+      ? this.textUtility.cropWithEllipsis(this.item.title, 60)
+      : '';
+  }
+
+  private get description(): string {
+    return this.item
+      ? this.textUtility.cropWithEllipsis(this.item.description, 280)
+      : '';
   }
 
 }
