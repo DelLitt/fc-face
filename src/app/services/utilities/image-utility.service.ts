8713489172bcc45a310bf3f218cdbf1db5afa44b
Eduard Karulis <karulis@mail.ru>
2018-02-22 12:30:24 +0300

    UTILITY. Image utility. Added supporting of empty image

--- src/app/services/utilities/image-utility.service.ts
+++ src/app/services/utilities/image-utility.service.ts
@@ -1,16 +1,41 @@
 import { Injectable } from '@angular/core';
 import { TextUtilityService } from './text-utility.service';
+import { LogService } from '../log.service';
+
+export const EMPTY_IMAGE_PATH = '/assets/img/_tmp/empty-img.png';
 
 @Injectable()
 export class ImageUtilityService {
 
-  constructor(private textUtility: TextUtilityService) { }
+  constructor(
+    private textUtility: TextUtilityService,
+    private logger: LogService
+  ) { }
+
+  public getImageAnyway(path: string, width: number = 0, height: number = 0) {
+    if (!path || path.length === 0) {
+      const alternatePath = this.getEmptyImage(width, height);
+      this.logger.logDebug(`'${(<any>this).constructor.name}' changed empty image path to alternate '${alternatePath}'`);
+      return alternatePath;
+    }
+
+    if (width === 0 || height === 0) {
+      return path;
+    }
+
+    return this.addFileVariantSize(path, width, height);
+  }
+
+  public getEmptyImage(width: number, height: number): string {
+    return this.addFileVariantSize(EMPTY_IMAGE_PATH, width, height);
+  }
 
   public addFileVariantSize(path: string, width: number, height: number): string {
     return this.addFileVariantKey(path, `${width}x${height}`);
   }
 
   public addFileVariantKey(path: string, variant: string): string {
+    this.logger.logDebug(`'${(<any>this).constructor.name}' applied variant ${variant} to image ${path}. Result: 'disabled'.`);
     return path;
 
     // TODO: Uncomment after integration with back end
