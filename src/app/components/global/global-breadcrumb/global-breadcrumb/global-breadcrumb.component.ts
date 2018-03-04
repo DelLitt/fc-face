bb911ec41d849c569bb3d538a8d8df18dbc7ff76
Eduard Karulis <karulis@mail.ru>
2018-03-04 22:53:58 +0300

    BREADCRUMB. Implemented dynamic generation

--- src/app/components/global/global-breadcrumb/global-breadcrumb/global-breadcrumb.component.ts
+++ src/app/components/global/global-breadcrumb/global-breadcrumb/global-breadcrumb.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { SiteMapItem, SiteMap } from '../../../../model/site-map';
+import { SiteMapService } from '../../../../services/site-map.service';
+import { LogService } from '../../../../services/log.service';
 
 @Component({
   selector: 'app-global-breadcrumb',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GlobalBreadcrumbComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private siteMapService: SiteMapService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private logger: LogService
+  ) { }
 
   ngOnInit() {
+  }
+
+  private get breadcrumbChain(): SiteMap {
+    return this.siteMapService.breadcrumbChain;
+  }
+
+  private get breadcrumbTitle(): string {
+    return this.siteMapService.breadcrumbTitle;
+  }
+
+  private get pageTitle(): string {
+    this.logger.logDebug(`'${(<any>this).constructor.name}' recieved title '${this.siteMapService.pageTitle}'`);
+    return this.siteMapService.pageTitle;
   }
 
 }
--- src/app/services/site-map.service.ts
+++ src/app/services/site-map.service.ts
@@ -1,23 +1,16 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRoute, Router, Routes } from '@angular/router';
+import { ActivatedRoute, Router, Routes, NavigationEnd } from '@angular/router';
 import { PageNotFoundComponent } from '../components/page-not-found/page-not-found.component';
 import { ContactsViewComponent } from '../components/contacts/contacts-view/contacts-view.component';
 import { PublicationViewComponent } from '../components/publications/publication-view/publication-view.component';
 import { HomeViewComponent } from '../components/main/home-view/home-view.component';
 import { SiteMap } from '../model/site-map';
+import { LogService } from './log.service';
 
-
-export const RoutesConfiguration: Routes = [
-  { path: 'home', component: HomeViewComponent },
-  { path: 'publications/:id', component: PublicationViewComponent },
-  { path: 'contacts', component: ContactsViewComponent },
-  { path: 'not-found', component: PageNotFoundComponent },
-  { path: '',   redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
-];
 
 const SiteMapConfiguration: SiteMap = [
   { defaultName: 'Home', i18nKey: 'HOME_PAGE', path: 'home', visible: true, clickable: true },
+  { defaultName: 'News', i18nKey: 'NEWS', path: 'publications', visible: false, clickable: true },
   { defaultName: 'Club', i18nKey: 'CLUB', path: 'club', visible: true, clickable: false,
       subItems: [
           { defaultName: 'Main team', i18nKey: 'MAIN_TEAM', path: 'club/mainteam', visible: true, clickable: true },
@@ -27,13 +20,73 @@ const SiteMapConfiguration: SiteMap = [
   { defaultName: 'Contacts', i18nKey: 'CONTACTS', path: 'contacts', visible: true, clickable: true }
 ];
 
+export const RoutesConfiguration: Routes = [
+  {
+    path: 'home',
+    component: HomeViewComponent
+  },
+  {
+    path: 'publications/:id',
+    component: PublicationViewComponent,
+    data: { routesChain: [ SiteMapConfiguration[0], SiteMapConfiguration[1] ], useTitle: true }
+  },
+  {
+    path: 'contacts',
+    component: ContactsViewComponent,
+    data: { routesChain: [ SiteMapConfiguration[0], SiteMapConfiguration[3] ], useTitle: false }
+  },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 @Injectable()
 export class SiteMapService {
 
-  constructor() { }
+  private _pageTitle: string;
+  private _breadcrumbTitle: string;
+  private _breadcrumbChain: SiteMap;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private logger: LogService
+  ) {
+    this.listenNavigation();
+  }
 
   public get items(): SiteMap {
     return SiteMapConfiguration;
   }
 
+  public get breadcrumbTitle(): string {
+    return this._breadcrumbTitle;
+  }
+
+  public set breadcrumbTitle(value) {
+    this._breadcrumbTitle = value;
+  }
+
+  public get breadcrumbChain(): SiteMap {
+    return this._breadcrumbChain;
+  }
+
+  public get pageTitle(): string {
+    return this._pageTitle;
+  }
+
+  public set pageTitle(value) {
+    this._pageTitle = value;
+  }
+
+  private listenNavigation() {
+    this.router.events
+    .filter(event => event instanceof NavigationEnd)
+    .subscribe((event) => this.buildBreadCrumb(this.activatedRoute));
+  }
+
+  private buildBreadCrumb(event) {
+    this._breadcrumbChain = event.snapshot.firstChild.data['routesChain'];
+    this._breadcrumbTitle = '';
+  }
 }
