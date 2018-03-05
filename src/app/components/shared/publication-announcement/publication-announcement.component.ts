67b7940a3314afad1bd3c8c9b753694eb988905c
Eduard Karulis <karulis@mail.ru>
2018-03-05 14:05:30 +0300

    NEWS. Added dynamic content to announcement

--- src/app/components/shared/publication-announcement/publication-announcement.component.ts
+++ src/app/components/shared/publication-announcement/publication-announcement.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Entity } from '../../../model/entity';
+import { LogService } from '../../../services/log.service';
+import { TextUtilityService } from '../../../services/utilities/text-utility.service';
+import { ImageUtilityService } from '../../../services/utilities/image-utility.service';
 
 @Component({
   selector: 'app-publication-announcement',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicationAnnouncementComponent implements OnInit {
 
-  constructor() { }
+  @Input() public item: Entity;
+  @Input() public routePart: string;
+
+  constructor(
+    private imageUtility: ImageUtilityService,
+    private textUtility: TextUtilityService,
+    private logger: LogService
+  ) { }
 
   ngOnInit() {
+  }
+
+  private get title(): string {
+    return this.item
+      ? this.textUtility.cropWithEllipsis(this.item.title, 250)
+      : '';
+  }
+
+  private get description(): string {
+    return this.item
+      ? this.textUtility.cropWithEllipsis(this.item.description, 500)
+      : '';
+  }
+
+  private getImage(width: number, height: number): string {
+    return this.imageUtility.getImageAnyway(this.item.img, width, height);
   }
 
 }
