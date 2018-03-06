49c7c9264cdb20c6d8ca3641bee1a577b86835b9
Eduard Karulis <karulis@mail.ru>
2018-03-06 19:06:26 +0300

    SEARCH. Implemented initial searching for search result comonent

--- src/app/components/search/search-results/search-results.component.ts
+++ src/app/components/search/search-results/search-results.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LogService } from '../../../services/log.service';
+import { Entity } from '../../../model/entity';
+import { SearchService } from '../../../services/search.service';
+
+const DefPageSize = 2;
 
 @Component({
   selector: 'app-search-results',
@@ -9,10 +13,18 @@ import { LogService } from '../../../services/log.service';
 })
 export class SearchResultsComponent implements OnInit {
 
+  private _loaded = false;
   private searchText = '';
+  private pageIndex = 0;
+  private pageSize: number = DefPageSize;
+  private pageSizeOptions: number[] = [DefPageSize, 25, 50, 100];
+  private entitiesTotalCount = 0;
+  private entities: Entity[];
 
   constructor(
+    private searchService: SearchService,
     private activatedRoute: ActivatedRoute,
+    private router: Router,
     private logger: LogService
   ) { }
 
@@ -23,8 +35,33 @@ export class SearchResultsComponent implements OnInit {
   private setSearchText() {
     this.activatedRoute.params.subscribe(params => {
       this.searchText = params['text'];
-      this.logger.logInfo(`'${(<any>this).constructor.name}' recieved text for searching: '${this.searchText}'.`);
+      this.processSearching();
+      this.logger.logError(`'${(<any>this).constructor.name}' has recieved text for searchingг г г: '${this.searchText}'.`);
    });
+  }
+
+  private processSearching() {
+    if (!this.search || this.searchText.length < 3 ) {
+      this.logger.logWarning(`'${(<any>this).constructor.name}' has recieved too short text for searching: '${this.searchText}'.`);
+      return;
+    }
+
+    this.search(10, 20);
+  }
+
+  private search(count, skip: number = 0) {
+    this.logger.logError(`'${(<any>this).constructor.name}' has started searching for ${this.searchText}.`);
+    this._loaded = false;
+
+    this.searchService.search(this.searchText)
+    .then(result => {
+      this.entities = result;
+      this._loaded = true;
+      this.logger.logError(`'${(<any>this).constructor.name}' has finished searching successfully.`);
+    })
+    .catch(reason => {
+      this.router.navigate(['/not-found']);
+    });
   }
 
 }
--- src/app/modules/search/search-components.module.ts
+++ src/app/modules/search/search-components.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchResultsComponent } from '../../components/search/search-results/search-results.component';
+import { SearchService } from '../../services/search.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatProgressSpinnerModule } from '@angular/material';
+import { SharedComponentsModule } from '../shared/shared-components.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatProgressSpinnerModule,
+    TranslateModule,
+    SharedComponentsModule
   ],
   declarations: [
     SearchResultsComponent
+  ],
+  providers: [
+    SearchService
   ],
   exports: [
     SearchResultsComponent
