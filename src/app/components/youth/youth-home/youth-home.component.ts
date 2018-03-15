e4af119cdcbb147975413991ec08e4d3518fe3d3
Eduard Karulis <karulis@mail.ru>
2018-03-15 18:04:12 +0300

    YOUTH. Added videos and galleries

--- src/app/components/youth/youth-home/youth-home.component.ts
+++ src/app/components/youth/youth-home/youth-home.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { PublicationsRepositoryService } from '../../../services/publications-repository.service';
 import { LogService } from '../../../services/log.service';
 import { Publication } from '../../../model/publication';
+import { VideosRepositoryService } from '../../../services/videos-repository.service';
+import { GalleriesRepositoryService } from '../../../services/galleries-repository.service';
+import { Video } from '../../../model/video';
+import { Gallery } from '../../../model/gallery/gallery';
 
 @Component({
   selector: 'app-youth-home',
@@ -13,27 +17,69 @@ export class YouthHomeComponent implements OnInit {
   private _actualPublicationsLoaded = false;
   private actualPublications: Publication[] = null;
   private publications: Publication[] = null;
+  private videos: Video[] = null;
+  private galleries: Gallery[] = null;
 
   constructor(
     private publicationsRepository: PublicationsRepositoryService,
+    private videosRepository: VideosRepositoryService,
+    private galleriesRepository: GalleriesRepositoryService,
     private logger: LogService
   ) { }
 
   ngOnInit() {
     this.logger.logDebug(`'${(<any>this).constructor.name}' component is being initialized.`);
     this.loadPublications();
+    this.loadVideos();
+    this.loadGalleries();
   }
 
   private loadPublications() {
     this.logger.logDebug(`'${(<any>this).constructor.name}' is trying to get publications.`);
 
-    this.publicationsRepository.getYouthPublications(7)
+    this.publicationsRepository.getYouthPublications(6)
       .then(
         result => {
           this.publications = result;
           this.actualPublications = this.publications.slice(0, 3);
           this._actualPublicationsLoaded = true;
           this.logger.logInfo(`'${(<any>this).constructor.name}' loaded publications. Count: ${this.publications.length}.`);
+        },
+        err => {
+          this.rollback(err);
+        }
+      )
+      .catch(reason => {
+        this.rollback(reason);
+      });
+  }
+
+  private loadVideos() {
+    this.logger.logDebug(`'${(<any>this).constructor.name}' is trying to get videos.`);
+
+    this.videosRepository.getYouthVideos(3)
+      .then(
+        result => {
+          this.videos = result;
+          this.logger.logInfo(`'${(<any>this).constructor.name}' loaded videos. Count: ${this.videos.length}.`);
+        },
+        err => {
+          this.rollback(err);
+        }
+      )
+      .catch(reason => {
+        this.rollback(reason);
+      });
+  }
+
+  private loadGalleries() {
+    this.logger.logDebug(`'${(<any>this).constructor.name}' is trying to get galleries.`);
+
+    this.galleriesRepository.getYouthGalleries(3)
+      .then(
+        result => {
+          this.galleries = result;
+          this.logger.logInfo(`'${(<any>this).constructor.name}' loaded galleries. Count: ${this.galleries.length}.`);
         },
         err => {
           this.rollback(err);
