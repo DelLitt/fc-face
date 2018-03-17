b3514561144373db82752a007a0c566899266dd6
Eduard Karulis <karulis@mail.ru>
2018-03-18 01:24:27 +0300

    FAKE DATA. Added fake data for testing

--- src/app/services/_fake/fake-employee-storage.ts
+++ src/app/services/_fake/fake-employee-storage.ts
@@ -1,8 +1,35 @@
 export const _fakeEmployees = [
-    {
-      id: 101,
-      img: '/assets/img/_tmp/person-01.png',
-      nameFirst: 'Александр',
-      nameLast: 'Ромашко'
-    }
+  {
+    id: 101,
+    img: '/assets/img/_tmp/person-01.png',
+    nameFirst: 'Александр',
+    nameLast: 'Ромашко',
+    role: {
+      id: 3,
+      title: 'coach',
+      i18nKey: 'COACH_DEF'
+      }
+  },
+  {
+    id: 102,
+    img: '/assets/img/_tmp/person-02.png',
+    nameFirst: 'Алексей',
+    nameLast: 'Тумбин',
+    role: {
+      id: 3,
+      title: 'coach',
+      i18nKey: 'COACH_DEF'
+      }
+  },
+  {
+    id: 104,
+    img: '/assets/img/_tmp/person-01.png',
+    nameFirst: 'Павел',
+    nameLast: 'Прищепало 221',
+    role: {
+      id: 3,
+      title: 'coach',
+      i18nKey: 'COACH_DEF'
+      }
+  }
 ];
---
+++ src/app/services/_fake/fake-youth-team-storage.ts
@@ -0,0 +1,33 @@
+export const _fakeYouthTeams = [
+  {
+    id: 101,
+    name: 'Слуцк-2003',
+    coach: 101
+  },
+  {
+    id: 102,
+    name: 'Слуцк-2004',
+    coach: 102
+  },
+  {
+    id: 103,
+    name: 'Слуцк-2005',
+    coach: 101
+  },
+  {
+    id: 104,
+    name: 'Слуцк-2006',
+    coach: 101
+  },
+  {
+    id: 105,
+    name: 'Слуцк-2007',
+    coach: 102
+  }
+  ,
+  {
+    id: 106,
+    name: 'Слуцк-2008',
+    coach: 104
+  }
+];
