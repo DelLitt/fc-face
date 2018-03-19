ba08bef2d623e7e874879972705d135158560cf8
Eduard Karulis <karulis@mail.ru>
2018-03-19 18:36:28 +0300

    YOUTH. Added dropdown to youth teams navigation

--- src/app/components/youth/youth-navigation/youth-navigation.component.ts
+++ src/app/components/youth/youth-navigation/youth-navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LogService } from '../../../services/log.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-youth-navigation',
@@ -9,11 +10,16 @@ import { LogService } from '../../../services/log.service';
 export class YouthNavigationComponent implements OnInit {
 
   constructor(
+    private router: Router,
     private logger: LogService
   ) { }
 
   ngOnInit() {
     this.logger.logDebug(`'${(<any>this).constructor.name}' component is being initialized.`);
+  }
+
+  private get isTeamsActive(): boolean {
+    return this.router.isActive('youth/teams', false);
   }
 
 }
