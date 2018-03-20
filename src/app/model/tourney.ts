cdf8965bd3017c3ba5fc22be3ade28f9d5b4eeb0
Eduard Karulis <karulis@mail.ru>
2018-03-21 02:41:19 +0300

    MODEL. Added tourney class

---
+++ src/app/model/tourney.ts
@@ -0,0 +1,9 @@
+export class Tourney {
+    public id: number;
+    public i18nKey: string;
+    public name: string;
+    public tourneyTypeId: number;
+    public cityId: number;
+    public dateStart: Date;
+    public dateEnd: Date;
+}
