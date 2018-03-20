926254e968c5d35e333852c69cebd84eca8eb912
Eduard Karulis <karulis@mail.ru>
2018-03-20 11:00:11 +0300

    YOUTH. Added loading team to youth teams component

--- src/app/components/youth/youth-teams/youth-teams.component.ts
+++ src/app/components/youth/youth-teams/youth-teams.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamsRepositoryService } from '../../../services/teams-repository.service';
+import { SiteMapService } from '../../../services/site-map.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LogService } from '../../../services/log.service';
+import { Team } from '../../../model/team';
 
 @Component({
   selector: 'app-youth-teams',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class YouthTeamsComponent implements OnInit {
 
-  constructor() { }
+  private _loaded: boolean;
+  private team: Team = new Team();
+  private _teamId: number;
+
+  constructor(
+    private teamsRepository: TeamsRepositoryService,
+    private siteMapService: SiteMapService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private logger: LogService
+  ) { }
 
   ngOnInit() {
+    this.logger.logWarning(
+      `'${(<any>this).constructor.name}'. ${this.siteMapService.getCurrentYouthTeamId(this.activatedRoute.snapshot)}'`);
+      this.loadTeam();
+
+      this.siteMapService.navigated.subscribe((event) => {
+        this.loadTeam();
+      });
+  }
+
+  private loadTeam() {
+    this._teamId = this.siteMapService.getCurrentYouthTeamId(this.activatedRoute.snapshot);
+
+    this.teamsRepository.getTeam(this._teamId)
+    .then(result => {
+      this.team = result;
+      this._loaded = true;
+      this.logger.logInfo(`'${(<any>this).constructor.name}' loaded the team (id:${this._teamId}) successfully.`);
+    })
+    .catch(reason => {
+      this.router.navigate(['/not-found']);
+    });
   }
 
 }
