3cd47a8f2a62ccff2a9ed41a066dbd289fbeadf0
Eduard Karulis <karulis@mail.ru>
2018-03-20 18:06:30 +0300

    MODEL. Added standing class

---
+++ src/app/model/standing.ts
@@ -0,0 +1,13 @@
+export class Standing {
+    public tourneyId: number;
+    public position: number;
+    public teamId: number;
+    public teamName: string;
+    public games: number;
+    public wins: number;
+    public draws: number;
+    public loses: number;
+    public goalsFor: number;
+    public goalsAgainst: number;
+    public points: number;
+  }
