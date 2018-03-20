2946b68d400f25a22126efc72b13a7eca9c6c6eb
Eduard Karulis <karulis@mail.ru>
2018-03-21 02:40:46 +0300

    FAKE DATA. Added fake tourney storage

---
+++ src/app/services/_fake/fake-tourneys-storage.ts
@@ -0,0 +1,32 @@
+export const _fakeTourneys = [
+    {
+        id: 15,
+        i18nKey: 'youth2003u15',
+        name: 'U-15. Group А',
+        tourneyTypeId: 1,
+        cityId: 1,
+        dateStart: new Date('Thu, 08 Jan 2017 15:34:12 GMT').toISOString(),
+        dateEnd: new Date('Thu, 08 Jan 2017 15:34:12 GMT').toISOString(),
+        teams: [2090, 2091, 2093]
+    },
+    {
+        id: 16,
+        i18nKey: 'youth2002u16',
+        name: 'U-16. Групп А',
+        tourneyTypeId: 1,
+        cityId: 1,
+        dateStart: new Date('Thu, 08 Jan 2017 15:14:11 GMT').toISOString(),
+        dateEnd: new Date('Thu, 08 Jan 2017 15:14:11 GMT').toISOString(),
+        teams: [2091, 2093]
+    },
+    {
+        id: 17,
+        i18nKey: 'youth2002u16',
+        name: 'U-16. Групп А',
+        tourneyTypeId: 1,
+        cityId: 1,
+        dateStart: new Date('Thu, 08 Jan 2017 12:35:18 GMT').toISOString(),
+        dateEnd: new Date('Thu, 08 Jan 2017 12:35:18 GMT').toISOString(),
+        teams: [2090, 2091]
+    }
+];
