c421c0dc6a54e86d7a7b941ca8faf3b23da3fb7c
Eduard Karulis <karulis@mail.ru>
2018-03-22 01:47:42 +0300

    YOUTH. Changed with inherited class

--- src/app/components/youth/teams/youth-team-results/youth-team-results.component.ts
+++ src/app/components/youth/teams/youth-team-results/youth-team-results.component.ts
@@ -1,64 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { LogService } from '../../../../services/log.service';
-import { Team } from '../../../../model/team';
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { SiteMapService } from '../../../../services/site-map.service';
-import { ActivatedRoute, Router } from '@angular/router';
 import { TeamsRepositoryService } from '../../../../services/teams-repository.service';
-import { Tourney } from '../../../../model/tourney';
+import { LogService } from '../../../../services/log.service';
+import { YouthTeamView } from '../youth-team-view';
 
 @Component({
   selector: 'app-youth-team-results',
   templateUrl: './youth-team-results.component.html',
   styleUrls: ['./youth-team-results.component.scss']
 })
-
-export class YouthTeamResultsComponent implements OnInit {
-  private _loaded: boolean;
-  private _teamId: number;
-  private _team: Team = new Team();
-  private _tourneyId: number;
-  private _tourney: Tourney = new Tourney();
+export class YouthTeamResultsComponent extends YouthTeamView {
 
   constructor(
-    private teamsRepository: TeamsRepositoryService,
-    private siteMapService: SiteMapService,
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private logger: LogService
-  ) { }
-
-  ngOnInit() {
-    this.logger.logDebug(`'${(<any>this).constructor.name}' component is being initialized.`);
-    this.loadTeam();
-  }
-
-  private loadTeam() {
-    this._loaded = false;
-    this._teamId = this.siteMapService.getCurrentYouthTeamId(this.activatedRoute.parent.snapshot);
-
-    this.teamsRepository.getTeam(this._teamId)
-    .then(result => {
-      this._team = result;
-      this._loaded = true;
-      this.logger.logInfo(`'${(<any>this).constructor.name}' loaded the team (id:${this._teamId}) successfully.`);
-    })
-    .catch(reason => {
-      this.router.navigate(['/not-found']);
-    });
-  }
-
-  public get tourneyId(): number {
-    return this._tourneyId;
-  }
-
-  @Input() public set tourneyId(value: number) {
-    this.logger.logError(`'${(<any>this).constructor.name}' setting tourneyId ${value}.`);
-    this._tourneyId = value;
-  }
-
-  private tourneyChanged(selectedTourney: Tourney) {
-    this.logger.logDebug(`'${(<any>this).constructor.name}'. Tourney changed to ${selectedTourney.id}.`);
-    this._tourney = selectedTourney;
+    protected teamsRepository: TeamsRepositoryService,
+    protected siteMapService: SiteMapService,
+    protected router: Router,
+    protected activatedRoute: ActivatedRoute,
+    protected logger: LogService
+  ) {
+    super(
+      teamsRepository,
+      siteMapService,
+      router,
+      activatedRoute,
+      logger
+    );
   }
 
 }
