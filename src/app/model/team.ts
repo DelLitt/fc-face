330a914c5cd3569d9d15d460ca8f7bc0cb7448ca
Eduard Karulis <karulis@mail.ru>
2018-03-22 14:11:01 +0300

    MODEL. Added implementation of 'SharedEntity' interface to class 'Team'

--- src/app/model/team.ts
+++ src/app/model/team.ts
@@ -1,5 +1,14 @@
-export class Team {
+import { SharedEntity } from './shared-entity';
+
+
+export class Team implements SharedEntity {
+    // SharedEntity
     public id: number;
+    public href: string;
     public name: string;
+    public i18nKey: string;
+    public img: string;
+
+    // Unique
     public coachId: number;
 }
