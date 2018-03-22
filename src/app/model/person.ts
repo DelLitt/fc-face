a7ac98e97d44de7d55113e83f57dfc6bfed3f13c
Eduard Karulis <karulis@mail.ru>
2018-03-22 14:38:05 +0300

    MODEL. Added implementation of 'SharedEntity' to class 'Team'

--- src/app/model/person.ts
+++ src/app/model/person.ts
@@ -1,11 +1,18 @@
 import { FcLabel } from './app/fc-label';
 import { Team } from './team';
+import { SharedEntity } from './shared-entity';
 
-export class Person {
+export class Person implements SharedEntity {
+    // SharedEntity
     public id: number;
+    public href: string;
+    public name: string;
+    public i18nKey: string;
+    public img: string;
+
+    // Unique
     public nameFirst: string;
     public nameLast: string;
-    public img: string;
     public role: FcLabel;
     public teams: Team[];
 }
