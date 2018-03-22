66ff00b64906abb69042f612516b65c67038f3b1
Eduard Karulis <karulis@mail.ru>
2018-03-22 18:17:40 +0300

    MODEL. Implamented interface 'SharedEntity' for the class 'Tourney'

--- src/app/model/tourney.ts
+++ src/app/model/tourney.ts
@@ -1,7 +1,14 @@
-export class Tourney {
+import { SharedEntity } from './shared-entity';
+
+export class Tourney implements SharedEntity {
+    // SharedEntity
     public id: number;
-    public i18nKey: string;
+    public href: string;
     public name: string;
+    public i18nKey: string;
+    public img: string;
+
+    // Unique
     public tourneyTypeId: number;
     public cityId: number;
     public dateStart: Date;
