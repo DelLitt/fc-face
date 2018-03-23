e8c4e74c4bd7be62775a518cab42f768a8f7aef8
Eduard Karulis <karulis@mail.ru>
2018-03-23 17:57:45 +0300

    YOUTH. Added loading tourneys to youth team page

--- src/app/components/youth/teams/youth-team-games/youth-team-games.component.ts
+++ src/app/components/youth/teams/youth-team-games/youth-team-games.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { SiteMapService } from '../../../../services/site-map.service';
 import { TeamsRepositoryService } from '../../../../services/teams-repository.service';
 import { LogService } from '../../../../services/log.service';
 import { YouthTeamView } from '../youth-team-view';
+import { TourneysRepositoryService } from '../../../../services/tourneys-repository.service';
+import { Tourney } from '../../../../model/tourney';
 
 
 @Component({
@@ -11,9 +13,13 @@ import { YouthTeamView } from '../youth-team-view';
   templateUrl: './youth-team-games.component.html',
   styleUrls: ['./youth-team-games.component.scss']
 })
-export class YouthTeamGamesComponent extends YouthTeamView {
+export class YouthTeamGamesComponent extends YouthTeamView implements OnInit {
+  private _loadedTourneys: boolean;
+  private _tourneys: Tourney[];
+  private _tourneyIds: number[] = new Array<number>();
 
   constructor(
+    private tourneysRepository: TourneysRepositoryService,
     protected teamsRepository: TeamsRepositoryService,
     protected siteMapService: SiteMapService,
     protected router: Router,
@@ -27,6 +33,28 @@ export class YouthTeamGamesComponent extends YouthTeamView {
       activatedRoute,
       logger
     );
+  }
+
+  ngOnInit() {
+    super.ngOnInit();
+    this.loadTourneys();
+  }
+
+  private loadTourneys() {
+    this._loadedTourneys = false;
+    this.tourneysRepository.getTeamTourneys(this._teamId)
+    .then(result => {
+      this._tourneys = result;
+      this.setTourneyIds();
+
+      this._loadedTourneys = true;
+      this.logger.logInfo(`'${(<any>this).constructor.name}' has loaded the tourneys of team (id:${this._teamId}) successfully.`);
+    });
+  }
+
+  private setTourneyIds() {
+    if (this._tourneys.length === 0) { return; }
+    this._tourneyIds = this._tourneys.map(t => t.id);
   }
 
 }
