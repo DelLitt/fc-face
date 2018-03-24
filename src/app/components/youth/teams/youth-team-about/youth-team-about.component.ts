aa5a60303aebf6a3486c5502c8466a4a3f454d45
Eduard Karulis <karulis@mail.ru>
2018-03-25 01:02:55 +0300

    YOUTH. Implemented logic

--- src/app/components/youth/teams/youth-team-about/youth-team-about.component.ts
+++ src/app/components/youth/teams/youth-team-about/youth-team-about.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { YouthTeamView } from '../youth-team-view';
+import { TeamsRepositoryService } from '../../../../services/teams-repository.service';
+import { SiteMapService } from '../../../../services/site-map.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { LogService } from '../../../../services/log.service';
+import { Team } from '../../../../model/teams/team';
+import { TeamStaticModel } from '../../../../model/teams/team-static-model';
 
 @Component({
   selector: 'app-youth-team-about',
   templateUrl: './youth-team-about.component.html',
   styleUrls: ['./youth-team-about.component.scss']
 })
-export class YouthTeamAboutComponent implements OnInit {
+export class YouthTeamAboutComponent extends YouthTeamView implements OnInit {
+  private _description: string = null;
 
-  constructor() { }
+  constructor(
+    protected teamsRepository: TeamsRepositoryService,
+    protected siteMapService: SiteMapService,
+    protected router: Router,
+    protected activatedRoute: ActivatedRoute,
+    protected logger: LogService
+  ) {
+    super(
+      teamsRepository,
+      siteMapService,
+      router,
+      activatedRoute,
+      logger
+    );
+  }
 
-  ngOnInit() {
+  private get description(): string {
+    if (!this._description) {
+      if (this._team instanceof Team && this._team.staticModel instanceof TeamStaticModel) {
+          this._description = this._team.staticModel.description;
+        }
+    }
+
+    return this._description;
   }
 
 }
