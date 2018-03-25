f84507148b396718c14c8601d5e6805508ac6bf2
Eduard Karulis <karulis@mail.ru>
2018-03-26 00:45:48 +0300

    RESULTS. Added base class of results pages

---
+++ src/app/components/results/results-view.ts
@@ -0,0 +1,29 @@
+import { OnInit } from '@angular/core';
+import { ConfigurationService } from '../../services/configuration/configuration.service';
+import { LogService } from '../../services/log.service';
+
+export class ResultsView implements OnInit {
+    protected _loaded: boolean;
+    protected _teamIds = new Array<number>();
+
+    constructor(
+        protected configuration: ConfigurationService,
+        protected logger: LogService
+      ) { }
+
+    ngOnInit() {
+        this.logger.logDebug(`'${(<any>this).constructor.name}' component is being initialized.`);
+        this.loadTeamIds();
+    }
+
+    private loadTeamIds() {
+        this.configuration.activeTeamsIds
+            .then(result => {
+                this._teamIds = result instanceof Array ? result : Array<number>();
+                this.configurationLoaded();
+            });
+    }
+
+    protected configurationLoaded() {
+    }
+}
