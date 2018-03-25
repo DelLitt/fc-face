4320d63faf0464ff715d97ac9a460fb4fafe8795
Eduard Karulis <karulis@mail.ru>
2018-03-25 08:39:10 +0300

    FAKE DATA. Changed for testing

--- src/app/services/_fake/fake-players-storage.ts
+++ src/app/services/_fake/fake-players-storage.ts
@@ -23,5 +23,40 @@ export const _fakeStaticPlayers = [
         id: 105,
         name: 'Павел Великолепенко',
         personNumber: 21
+    },
+    {
+        id: 106,
+        name: 'Семен Иванов',
+        personNumber: 4
+    },
+    {
+        id: 107,
+        name: 'Дмтрий Тараканников',
+        personNumber: 5
+    },
+    {
+        id: 108,
+        name: 'Андрей Драконий',
+        personNumber: 6
+    },
+    {
+        id: 109,
+        name: 'Иллионор Бисквитов',
+        personNumber: 7
+    },
+    {
+        id: 110,
+        name: 'Павел Великолепенко',
+        personNumber: 8
+    },
+    {
+        id: 111,
+        name: 'Иллионор Бисквитов',
+        personNumber: 9
+    },
+    {
+        id: 112,
+        name: 'Павел Великолепенко',
+        personNumber: 10
     }
 ];
--- src/app/services/_fake/fake-youth-team-storage.ts
+++ src/app/services/_fake/fake-youth-team-storage.ts
@@ -30,20 +30,47 @@ export const _fakeYouthTeams = [
         _fakeStaticPlayers[1],
         _fakeStaticPlayers[2],
         _fakeStaticPlayers[3],
-        _fakeStaticPlayers[4]
+        _fakeStaticPlayers[4],
+        _fakeStaticPlayers[5],
+        _fakeStaticPlayers[6],
+        _fakeStaticPlayers[7],
+        _fakeStaticPlayers[8],
+        _fakeStaticPlayers[9],
+        _fakeStaticPlayers[10],
+        _fakeStaticPlayers[11]
+      ],
+      coaches: [
+        _fakeEmployees[0],
+        _fakeEmployees[1]
       ]
-    },
-    coaches: [
-      _fakeEmployees[0],
-      _fakeEmployees[1]
-    ]
+    }
   },
   {
     id: 2091,
     name: 'Слуцк-2007',
-    coach: 102
-  }
-  ,
+    coach: 102,
+    staticModel: {
+      img: '/assets/img/_tmp/img-slide-2.png',
+      description: _fakeTextDescription_1,
+      players: [
+        _fakeStaticPlayers[3],
+        _fakeStaticPlayers[0],
+        _fakeStaticPlayers[2],
+        _fakeStaticPlayers[1],
+        _fakeStaticPlayers[4],
+        _fakeStaticPlayers[5],
+        _fakeStaticPlayers[6],
+        _fakeStaticPlayers[7],
+        _fakeStaticPlayers[8],
+        _fakeStaticPlayers[9],
+        _fakeStaticPlayers[10],
+        _fakeStaticPlayers[11]
+      ],
+      coaches: [
+        _fakeEmployees[1]
+      ]
+    }
+  },
   {
     id: 2090,
     name: 'Слуцк-2008',
