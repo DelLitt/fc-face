e04a4d6e35169ca3d11a672a67e807f5ac1e138c
Eduard Karulis <karulis@mail.ru>
2018-03-26 18:40:09 +0300

    FAKE DATA. Added fake data

--- src/app/services/_fake/fake-players-statistics.ts
+++ src/app/services/_fake/fake-players-statistics.ts
@@ -1,31 +1,31 @@
 import { _fakeStaticPlayers } from './fake-players-storage';
 
 export const _fakePlayersStatistics = [
-    { person: _fakeStaticPlayers[0], teamId: 2093, tourneyId: 15,
+    { person: _fakeStaticPlayers[0], teamId: 2093, tourneyId: 15, addIntValue: 0,
         games: 12, assists: 2, goals: 4, substitutes: 3, yellows: 1, reds: 0
     },
-    { person: _fakeStaticPlayers[0], teamId: 2091, tourneyId: 16,
+    { person: _fakeStaticPlayers[0], teamId: 2091, tourneyId: 16, addIntValue: 1,
         games: 11, assists: 0, goals: 0, substitutes: 0, yellows: 0, reds: 0
     },
-    { person: _fakeStaticPlayers[1], teamId: 2093, tourneyId: 15,
+    { person: _fakeStaticPlayers[1], teamId: 2093, tourneyId: 15, addIntValue: 0,
         games: 17, assists: 1, goals: 2, substitutes: 1, yellows: 4, reds: 0
     },
-    { person: _fakeStaticPlayers[1], teamId: 2093, tourneyId: 16,
+    { person: _fakeStaticPlayers[1], teamId: 2093, tourneyId: 16, addIntValue: 2,
         games: 15, assists: 5, goals: 9, substitutes: 5, yellows: 3, reds: 0
     },
-    { person: _fakeStaticPlayers[2], teamId: 2093, tourneyId: 15,
+    { person: _fakeStaticPlayers[2], teamId: 2093, tourneyId: 15, addIntValue: 0,
         games: 20, assists: 1, goals: 0, substitutes: 0, yellows: 3, reds: 1
     },
-    { person: _fakeStaticPlayers[2], teamId: 2093, tourneyId: 16,
+    { person: _fakeStaticPlayers[2], teamId: 2093, tourneyId: 16, addIntValue: 0,
         games: 17, assists: 0, goals: 3, substitutes: 0, yellows: 0, reds: 0
     },
-    { person: _fakeStaticPlayers[3], teamId: 2093, tourneyId: 15,
+    { person: _fakeStaticPlayers[3], teamId: 2093, tourneyId: 15, addIntValue: 0,
         games: 17, assists: 3, goals: 6, substitutes: 2, yellows: 3, reds: 0
     },
-    { person: _fakeStaticPlayers[4], teamId: 2093, tourneyId: 15,
+    { person: _fakeStaticPlayers[4], teamId: 2093, tourneyId: 15, addIntValue: 0,
         games: 19, assists: 1, goals: 5, substitutes: 1, yellows: 4, reds: 0
     },
-    { person: _fakeStaticPlayers[4], teamId: 2093, tourneyId: 16,
+    { person: _fakeStaticPlayers[4], teamId: 2093, tourneyId: 16, addIntValue: 0,
         games: 20, assists: 0, goals: 0, substitutes: 0, yellows: 5, reds: 2
     }
 ];
--- src/app/services/_fake/fake-players-storage.ts
+++ src/app/services/_fake/fake-players-storage.ts
@@ -3,72 +3,109 @@ export const _fakeStaticPlayers = [
         id: 101,
         name: 'Семен Иванов',
         personNumber: 1,
-        birthDate: new Date('08 Jan 1994').toISOString()
+        birthDate: new Date('08 Jan 1994').toISOString(),
+        nameFirst: 'Семен',
+        nameLast: 'Иванов',
+        age: 24
     },
     {
         id: 102,
         name: 'Дмтрий Тараканников',
         personNumber: 2,
-        birthDate: new Date('08 Jan 1991').toISOString()
+        birthDate: new Date('08 Jan 1991').toISOString(),
+        nameFirst: 'Дмтрий',
+        nameLast: 'Тараканников',
+        age: 27
     },
     {
         id: 103,
         name: 'Андрей Драконий',
         personNumber: 3,
-        birthDate: new Date('08 Jan 1988').toISOString()
+        birthDate: new Date('08 Jan 1988').toISOString(),
+        nameFirst: 'Андрей',
+        nameLast: 'Драконий',
+        age: 30,
+        personRoleId: 31
     },
     {
         id: 104,
         name: 'Иллионор Бисквитов',
         personNumber: 13,
-        birthDate: new Date('08 Jan 1999').toISOString()
+        birthDate: new Date('08 Jan 1999').toISOString(),
+        nameFirst: 'Иллионор',
+        nameLast: 'Бисквитов',
+        age: 28
     },
     {
         id: 105,
         name: 'Павел Великолепенко',
         personNumber: 21,
-        birthDate: new Date('08 Jan 2000').toISOString()
+        birthDate: new Date('08 Jan 2000').toISOString(),
+        nameFirst: 'Павел',
+        nameLast: 'Великолепенко',
+        age: 18
     },
     {
         id: 106,
-        name: 'Семен Иванов',
+        name: 'Игорь Иванов',
         personNumber: 4,
-        birthDate: new Date('08 Jan 1996').toISOString()
+        birthDate: new Date('08 Jan 1996').toISOString(),
+        nameFirst: 'Игорь',
+        nameLast: 'Иванов',
+        age: 22
     },
     {
         id: 107,
-        name: 'Дмтрий Тараканников',
+        name: 'Дмтрий Лозкин',
         personNumber: 5,
-        birthDate: new Date('08 Jan 1996').toISOString()
+        birthDate: new Date('08 Jan 1996').toISOString(),
+        nameFirst: 'Дмтрий',
+        nameLast: 'Лозкин',
+        age: 22
     },
     {
         id: 108,
-        name: 'Андрей Драконий',
+        name: 'Андрей Петровичев',
         personNumber: 6,
-        birthDate: new Date('08 Jan 1992').toISOString()
+        birthDate: new Date('08 Jan 1992').toISOString(),
+        nameFirst: 'Андрей',
+        nameLast: 'Петровичев',
+        age: 26
     },
     {
         id: 109,
         name: 'Иллионор Бисквитов',
         personNumber: 7,
-        birthDate: new Date('08 Jan 1990').toISOString()
+        birthDate: new Date('08 Jan 1990').toISOString(),
+        nameFirst: 'Семен',
+        nameLast: 'Иванов',
+        age: 28
     },
     {
         id: 110,
         name: 'Павел Великолепенко',
         personNumber: 8,
-        birthDate: new Date('08 Jan 1993').toISOString()
+        birthDate: new Date('08 Jan 1993').toISOString(),
+        nameFirst: 'Семен',
+        nameLast: 'Иванов',
+        age: 25
     },
     {
         id: 111,
         name: 'Иллионор Бисквитов',
         personNumber: 9,
-        birthDate: new Date('08 Jan 1994').toISOString()
+        birthDate: new Date('08 Jan 1994').toISOString(),
+        nameFirst: 'Семен',
+        nameLast: 'Иванов',
+        age: 24
     },
     {
         id: 112,
         name: 'Павел Великолепенко',
         personNumber: 10,
-        birthDate: new Date('08 Jan 1989').toISOString()
+        birthDate: new Date('08 Jan 1989').toISOString(),
+        nameFirst: 'Семен',
+        nameLast: 'Иванов',
+        age: 29
     }
 ];
