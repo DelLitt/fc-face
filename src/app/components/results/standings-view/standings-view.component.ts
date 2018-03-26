bac880cacbafa85f1cfb5a4e7b782d908c8a6f2a
Eduard Karulis <karulis@mail.ru>
2018-03-26 12:24:35 +0300

    STANDINGS. Added standings view component template

---
+++ src/app/components/results/standings-view/standings-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-standings-view',
+  templateUrl: './standings-view.component.html',
+  styleUrls: ['./standings-view.component.scss']
+})
+export class StandingsViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
