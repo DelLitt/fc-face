c55294bd586d1848a6b0858a0e66e5fdf7600fa4
Eduard Karulis <karulis@mail.ru>
2018-03-27 01:15:52 +0300

    DIRECTION VIEW. Added component template

---
+++ src/app/components/club/direction-view/direction-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-direction-view',
+  templateUrl: './direction-view.component.html',
+  styleUrls: ['./direction-view.component.scss']
+})
+export class DirectionViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
