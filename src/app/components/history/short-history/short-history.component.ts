21125622ea69b2fe1aba0898cdf1ba50bf77f450
Eduard Karulis <karulis@mail.ru>
2018-03-28 10:08:50 +0300

    HISTORY. Added short history component

---
+++ src/app/components/history/short-history/short-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-short-history',
+  templateUrl: './short-history.component.html',
+  styleUrls: ['./short-history.component.scss']
+})
+export class ShortHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
